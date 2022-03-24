8b08b6a4f065d53c66d8894e05fa1715ce12ac7f
ohman-23 <ethan.johman@gmail.com>
2022-03-24 13:06:58 -0500

    - added argument parsing to run server - type of server that is run can now be controlled from the command line with argument parsing

--- waypoint_server_2/application.py
+++ waypoint_server_2/application.py
@@ -1,6 +1,22 @@
 from app import create_app
+import sys
+from app.config import ConfigType
+import argparse
 
-application = create_app()
+parser = argparse.ArgumentParser()
+parser.add_argument("--run_type", type=str, required=False)
+args = parser.parse_args()
+
+def retrieve_config(args):
+    if not args.run_type: 
+        return ConfigType.DEVELOPMENT
+    config_type_dict = {'dev':ConfigType.DEVELOPMENT, 'deploy':ConfigType.DEPLOYMENT, 'test':ConfigType.TESTING}
+    config_type = config_type_dict.get(args.run_type, None)
+    if not config_type:
+        sys.exit("[ERROR] - select a valid run_type: [dev deploy test] in order to run server")
+    return config_type
+
+application = create_app(conf_type=retrieve_config(args))
 
 if __name__ == "__main__":
     application.run(host=application.config["HOST"], port=application.config["PORT"], debug=True)
